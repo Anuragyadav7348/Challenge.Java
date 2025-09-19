232144dc5f41e71914b246a61e88cb338b28f373
Anurag Yadav <anuragvnm01@gmail.com>
2025-09-19 16:37:52 +0530

    initial commit

---
+++ challenge java by prashant sir/Array1.java
@@ -0,0 +1,27 @@
+import java .util.Scanner;
+public class Array1 {
+     public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        int [] arr = new int [5];
+         arr[0] = 23;
+         arr[1] =45;
+         arr[2] =233;
+        arr [3] = 333;
+        arr[4] = 3;
+        System.out.println(arr[3]);
+
+        //input using for loop
+        for ( int i = 0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+
+        }
+for (int i=0; i<arr.length; i++) {
+    System.out.println(arr[2]+ "");
+
+}
+        
+
+    }
+    
+}
+

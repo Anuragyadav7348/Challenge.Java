a33f9b7e78cf64cdf52625aa7e2a7a11c0dc7128
Anurag Yadav <anuragvnm01@gmail.com>
2025-09-27 19:45:16 +0530

    initaily commit

---
+++ .vscode/Arraylist.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(10);
+        
+        list.add(76);
+        list.add(78);
+
+        list.add(68);
+
+        list.add(96);
+
+        list.add(46);
+        
+System.err.println(list);
+System.err.println(list.contains(46));
+    }
+
+
+
+    
+}

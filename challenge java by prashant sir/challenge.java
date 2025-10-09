c9446f79f7105c7bae9d10a26d3fef53e544f8a7
Anurag Yadav <anuragvnm01@gmail.com>
2025-10-09 19:25:49 +0530

    first  commit

--- challenge java by prashant sir/challenge.java
+++ challenge java by prashant sir/challenge.java
@@ -3,11 +3,11 @@ public class challenge {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
         System.err.println("calculate simple intrest");
-        System.out.print("enter your principle amount Rs");
+        System.out.print("enter  principle amount Rs");
         int Principle = input .nextInt();
-        System.out.print("enter your rate amount Rs");
+        System.out.print("enter  rate amount Rs");
         float Rate = input .nextFloat();
-        System.out.print("enter your Year amount Rs");
+        System.out.print("enter  Year amount Rs");
         float years =  input.nextFloat();
 
         float intrest =( Principle * Rate * years);
